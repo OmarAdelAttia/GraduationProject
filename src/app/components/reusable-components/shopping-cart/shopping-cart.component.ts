af766b404afe7fa555cebd112b42ab3d2e3f8ed9
nourabdelhakam <nour.moh93@gmail.com>
2021-04-23 23:54:06 +0200

    qw

--- src/app/components/reusable-components/shopping-cart/shopping-cart.component.ts
+++ src/app/components/reusable-components/shopping-cart/shopping-cart.component.ts
@@ -10,17 +10,27 @@ import { removeFromCart } from '../../shared/services/store/cart.action';
 export class ShoppingCartComponent implements OnInit {
 
   items;
+  u;
   constructor(private store: Store<{cart}>) { }
 
   ngOnInit(): void {
     this.store.select('cart').subscribe((data) => {
       this.items = data;
+      // console.log(this.items.length);
+
     })
+
+    this.u = this.store.select('cart');
+
   }
 
   removeFromShoppingCart = (item) => {
     let arrayItems = this.items.cartItems.filter(x => x.id != item.id);
     this.store.dispatch(new removeFromCart(arrayItems))
+  }
+
+  userCheckOutForm(checkOutForm) {
+    console.log(checkOutForm);
   }
 
 }
