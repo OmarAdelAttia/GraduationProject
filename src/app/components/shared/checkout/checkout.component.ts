5ad16ca80bc96230c0380f9729d872414d72089e
nourabdelhakam <nour.moh93@gmail.com>
2021-04-23 16:22:30 +0200

    checkout2

--- src/app/components/shared/checkout/checkout.component.ts
+++ src/app/components/shared/checkout/checkout.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
+import { ActivatedRoute, Router } from '@angular/router';
+import { TopRatedService } from '../../../components/shared/services/Apis/top-rated.service';
+import { DoctorsService } from '../../../components/shared/services/Apis/doctors.service';
+import { TrainersService } from '../../../components/shared/services/Apis/trainers.service';
+
 // angular from stepper
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
@@ -19,10 +24,47 @@ export class CheckoutComponent implements OnInit {
   thirdFormGroup!: FormGroup;
   forthFormGroup!: FormGroup;
 
+  activeId;
+  mentor;
 
-  constructor(private modalService: NgbModal, private _formBuilder: FormBuilder) { }
+  constructor(private modalService: NgbModal, private _formBuilder: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    public topRatedService: TopRatedService,
+    private doctorsService: DoctorsService,
+    private trainersService: TrainersService,) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.activeId = params;
+      console.log(params.id);
+
+      this.doctorsService.getDoctor(params.id).subscribe(doctor => {
+        this.mentor = doctor;
+        // console.log(doctor)
+        console.log(this.mentor)
+      });
+
+
+
+
+
+      this.trainersService.getTrainer(params.id).subscribe(trainer => {
+        this.mentor = trainer;
+        // console.log(trainer)
+        // console.log(this.mentor)
+      });
+
+
+
+      this.topRatedService.getTopRated(params.id).subscribe(topRated => {
+        this.mentor = topRated;
+        // console.log(topRated)
+        console.log(this.mentor)
+      });
+    })
+      // console.log(this.activeId);
+
     // angular from stepper
     this.firstFormGroup = this._formBuilder.group({
       firstCtrl: ['', Validators.required]
